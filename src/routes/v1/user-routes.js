573b5702e30ba199763d8ed4ac357344f3309699
hritik kumar <hritik.7827@gmail.com>
2024-05-01 18:49:18 +0530

    update user routes

--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { userController } = require("../../controllers");
 
-router.post("/", userController.userSignup);
+router.post("/signup", userController.userSignup);
 
 module.exports = router;
