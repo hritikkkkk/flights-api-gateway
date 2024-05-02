4e95128a5bcd86fff38066101e82755b101e2808
hritik kumar <hritik.7827@gmail.com>
2024-05-02 18:38:21 +0530

    added rate limiter and proxy

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -6,4 +6,6 @@ module.exports = {
   PORT: process.env.PORT,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY,
+  FLIGHT_SERVER: process.env.FLIGHT_SERVER,
+  BOOKING_SERVER: process.env.BOOKING_SERVER,
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-const express = require('express');
-
-const { ServerConfig } = require('./config');
-const apiRoutes = require('./routes');
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const { ServerConfig } = require("./config");
+const apiRoutes = require("./routes");
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 50 requests per `window` (here, per 15 minutes).
+});
 
 app.use(express.json());
 app.use(
@@ -11,9 +17,28 @@ app.use(
     extended: true,
   })
 );
+app.use(limiter);
 
-app.use('/api', apiRoutes);
+app.use(
+  "/flightsService",
+  createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVER,
+    changeOrigin: true,
+    pathRewrite: { "^/flightsService": "/" },
+  })
+);
+
+app.use(
+  "/BookingsService",
+  createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVER,
+    changeOrigin: true,
+    pathRewrite: { "^/bookingsService": "/" },
+  })
+);
+
+app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
-    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+  console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
